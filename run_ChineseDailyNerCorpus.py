1d32ffa6b4d7033e509a80b49675f38c14f3dc30
danan0755 <236237606@qq.com>
2020-04-12 18:54:23 +0800

    基于kashgari实现ner

---
+++ run_ChineseDailyNerCorpus.py
@@ -0,0 +1,10 @@
+from kashgari.corpus import ChineseDailyNerCorpus
+from kashgari.tasks.labeling import BiLSTM_Model
+
+train_x, train_y = ChineseDailyNerCorpus.load_data('train')
+test_x, test_y = ChineseDailyNerCorpus.load_data('test')
+valid_x, valid_y = ChineseDailyNerCorpus.load_data('valid')
+
+model = BiLSTM_Model()
+model.fit(train_x, train_y, valid_x, valid_y, epochs=50)
+
